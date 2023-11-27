ff669b0378701c8a629c69e4678793732ace51ef
Ganhkhuyag <gankhuyaggamba@gmail.com>
2023-11-27 12:47:15 -0800

    firts commit

---
+++ first.js
@@ -0,0 +1,75 @@
+let firstName = "Ganbaa";
+// let myAge = 22;
+
+console.log(firstName);
+console.log(myAge);
+
+let num1 = 10;
+let num2 = 20;
+
+// let sum = num1 + num2;
+let product = num1 * num2;
+let division = num1 / num2;
+let subtraction = num1 - num2;
+
+console.log(sum);
+console.log(product);
+console.log(division);
+console.log(subtraction);
+
+// y = x*x + 2*x + 1;
+// let x = 5;
+let answer = (y = x * x + 2 * x + 1);
+console.log(answer);
+
+let number1 = 10;
+let number2 = 20;
+let sum = number1 + number2;
+console.log(sum);
+
+let x = 15;
+let y = 20;
+if (x > y) {
+  console.log("x is bigger");
+} else {
+  console.log("y is bigger");
+}
+
+let myAge = 20;
+// hervee minii nas 21s baga bol baar orj bolohgvi.
+if (myAge < 21) {
+  console.log("you are not allowed to enter:", myAge);
+} else {
+  console.log("you are allowed to enter:", myAge);
+}
+
+let myBrithYear = 1954;
+console.log(2023 - myBrithYear);
+
+let myNumber = 30;
+console.log("myNumber=", myNumber);
+
+let driver = 17;
+// 18 hursen bol mashin barij bolno. hureegvi bol mashin barij bolohgvi.
+if (driver < 18) {
+  console.log("bolno");
+} else {
+  console.log("bolohgvi");
+}
+
+let day = 7;
+if (1 == day) {
+  console.log("Monday");
+} else if (2 == day) {
+  console.log("tuesday");
+} else if (3 == day) {
+  console.log("wednesday");
+} else if (4 == day) {
+  console.log("thuesday");
+} else if (5 == day) {
+  console.log("Friday");
+} else if (6 == day) {
+  console.log("saturday");
+} else if (7 == day) {
+  console.log("Sunday");
+}
