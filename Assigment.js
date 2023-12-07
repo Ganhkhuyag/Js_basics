f701839cc84d65f78be1514b7ea2517b82ce65c4
Apple <apple@Apples-MacBook-Air.local>
2023-12-07 16:57:35 +0800

    commit1

---
+++ Assigment.js
@@ -0,0 +1,26 @@
+// 1. 
+
+function y(n) {
+  for (i = n; i >= 0; i = i - 3) {
+    console.log(i);
+  }
+}
+y(100);
+
+// 2. 
+
+function y(n1, n2) {
+  for (i = n2; i <= n1; i = i + 1) {
+    console.log(i);
+  }
+}
+y(150, 90);
+
+// 3. 
+
+function y(a,b,c) {
+    for(i=){
+
+    }
+}
+y(10,20,2)
